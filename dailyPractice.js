defa764be2ec06d33c12fb21d584f8dbd7fe8390
Chris McKnight <tenlongfingers10@gmail.com>
2022-08-16 18:54:43 -0600

    lawl I feel dumb

--- dailyPractice.js
+++ dailyPractice.js
@@ -1,12 +1,8 @@
 // Write a function that takes an array (a) and a number (n) as arguments
 // It should return the last n elements of a
-//Still broken, but seriously, I need to go back to homework now
+
 numArr = [1, 2, 3, 4, 5];
 function myFunction(a, n) {
-  const lastItemInArray = Math.max(...a);
-  for (let i = n; i > 0; i--) {
-    return lastItemInArray - i;
-  }
-  console.log(lastItemInArray);
+  return a.slice(a.length - n);
 }
 console.log(myFunction(numArr, 2));
